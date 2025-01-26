3c7c4219f923b57703d1576e56e5428e269f3f11
SABBELLA GANESWARA REDDY <sunny@SABBELLAs-Air.bbrouter>
2025-01-26 15:22:07 +0530

    sample react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello Ganesh 
+        </p>
+        <h1>Welcome of Devops Trainings</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
